fc5d43df4e7309b96bd3ea43d7e1039fb3cd81b9
Martin Tsenev <145794822+MartinTs168@users.noreply.github.com>
2023-09-23 20:56:57 +0300

    Added the option to play again and a score keeping system

--- rock_paper_scisssors.py
+++ rock_paper_scisssors.py
@@ -4,22 +4,32 @@ from colorama import Fore
 rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
-player_move = input("Choose [r]ock, [p]aper, [s]cissors: ")
-if player_move == "r":
-    player_move = rock
-elif player_move == "p":
-    player_move = paper
-elif player_move == "s":
-    player_move = scissors
-else:
-    raise SystemExit("Invalid Input. Try again...")
+computer_games_won = 0
+player_games_won = 0
+drawn_games = 0
+while True:
+    player_move = input("Choose [r]ock, [p]aper, [s]cissors: ")
+    if player_move == "r":
+        player_move = rock
+    elif player_move == "p":
+        player_move = paper
+    elif player_move == "s":
+        player_move = scissors
+    else:
+        raise SystemExit("Invalid Input. Try again...")
+    computer_move = random.choice([rock, paper, scissors])
+    print(f"The computer chose: {computer_move}")
+    if player_move == computer_move:
+        print(Fore.YELLOW + "Draw")
+        drawn_games += 1
+    elif (player_move == rock and computer_move == scissors) or (player_move == paper and computer_move == rock) \
+            or (player_move == scissors and computer_move == paper):
+        print(Fore.GREEN + "You Win")
+        player_games_won += 1
+    else:
+        print(Fore.RED + "You Lose")
+        computer_games_won += 1
+    print(Fore.WHITE + f"You: {player_games_won} || Computer: {computer_games_won} || Draws: {drawn_games}")
+    if input("Play again? [y/n]: ").lower() == "n":
+        break
 
-computer_move = random.choice([rock, paper, scissors])
-print(f"The computer chose: {computer_move}")
-if player_move == computer_move:
-    print(Fore.YELLOW + "Draw")
-elif (player_move == rock and computer_move == scissors) or (player_move == paper and computer_move == rock) \
-        or (player_move == scissors and computer_move == paper):
-    print(Fore.GREEN + "You Win")
-else:
-    print(Fore.RED + "You Lose")
