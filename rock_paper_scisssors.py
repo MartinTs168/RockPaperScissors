76bd5f72f35a5aaa418a364b319db0d8a175bfd4
Martin Tsenev <145794822+MartinTs168@users.noreply.github.com>
2023-09-23 19:38:34 +0300

    Source code of the game

---
+++ rock_paper_scisssors.py
@@ -0,0 +1,23 @@
+import random
+rock = "Rock"
+paper = "Paper"
+scissors = "Scissors"
+player_move = input("Choose [r]ock, [p]aper, [s]cissors: ")
+if player_move == "r":
+    player_move = rock
+elif player_move == "p":
+    player_move = paper
+elif player_move == "s":
+    player_move = scissors
+else:
+    raise SystemExit("Invalid Input. Try again...")
+
+computer_move = random.choice([rock, paper, scissors])
+print(f"The computer chose: {computer_move}")
+if player_move == computer_move:
+    print("Draw")
+elif (player_move == rock and computer_move == scissors) or (player_move == paper and computer_move == rock) \
+        or (player_move == scissors and computer_move == paper):
+    print("You Win")
+else:
+    print("You Lose")
